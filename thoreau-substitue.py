3279cdd8c5c6eb9989693bcacb7da02b87776c75
Jarmo Hietala <68544023+hietjmo@users.noreply.github.com>
2021-09-14 14:24:47 +0200

    Add files via upload

---
+++ thoreau-substitue.py
@@ -0,0 +1,68 @@
+
+# python thoreau-substitue.py -i dictionario-encyclopedic-2021-08-31.txt -o output.txt --log
+
+"""
+Haiku VI
+
+Aride stratas
+ma in alcun fenestras
+yo vide flores
+
+(Marcus Scriptor, 2013)
+"""
+
+import argparse
+import re
+import time
+from math import *
+from collections import defaultdict
+from tabulate import tabulate
+
+def read_args ():
+  parser = argparse.ArgumentParser ()
+  parser.add_argument ("-i", "--input", default="input.txt")
+  parser.add_argument ("-o", "--output", default="output.txt")
+  parser.add_argument ("-s", "--substitue", default="substitue.txt")
+  parser.add_argument ("-sep", "--separator", default="\t")
+  parser.add_argument ("--log", action="store_true")
+  args = parser.parse_args ()
+  return (args)
+
+args = read_args()
+
+start = time.time()
+
+g = open (args.substitue)
+xs = g.readlines ()
+g.close ()
+
+subs = []
+for x in xs:
+  sb = x.strip ("\n").split (args.separator)
+  if len (sb) == 2:
+    subs.append (sb)
+
+f = open (args.input)
+text = f.read ()
+f.close ()
+
+found = defaultdict (int) # default = 0
+for pattern,repl in subs:
+  text,fnd = re.subn (pattern,repl,text)
+  found [(pattern,repl)] = found [(pattern,repl)] + fnd
+
+h = open (args.output,"w")
+h.write (text)
+h.close ()
+
+subs2 = []
+for a,(k,v) in zip (subs,found.items()):
+  subs2.append (a + [v])
+
+end = time.time()
+total = end - start
+decimals = abs (floor (log (total,10))) + 2
+if args.log:
+  print (tabulate (subs2))
+  print (f"({total:.{decimals}} s)\n") 
+
